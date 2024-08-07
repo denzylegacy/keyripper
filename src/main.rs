bbbe875c09118fd91f94b708697678f5b581fa7d
Denzy Legacy <alissondin.developer@gmail.com>
2024-08-06 21:02:04 -0300

    secp256k1 4.5k p 1s

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 use bitcoin::secp256k1::Secp256k1;
 use std::time::{Instant};
+=======
+#![allow(unused)]
+
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
+
+// use bitcoin::secp256k1::Secp256k1;
+>>>>>>> Stashed changes
 use std::sync::{Arc, Mutex};
 use std::thread;
 
@@ -14,10 +22,18 @@ use key_search_approaches::ascending_search::ascending_search;
 fn main() {
     introduction();
 
+<<<<<<< Updated upstream
     let min_key_range: u128 = 0x20000000000000000;
     let max_key_range: u128 = 0x3ffffffffffffffff;
     let target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so";
     let status_output_timer = 10;
+=======
+    let min_key_range: u32 = 0x800;
+    let max_key_range: u32 = 0xfff;
+    let target_address: &str = "1DBaumZxUkM4qMQRt2LVWyFJq5kDtSZQot";
+
+    let status_output_timer = 10u64;
+>>>>>>> Stashed changes
 
     println!("[+] Loading...\n");
 
@@ -31,6 +47,7 @@ fn main() {
     let last_report_time = Arc::new(Mutex::new(Instant::now()));
     let keys_checked_in_interval = Arc::new(Mutex::new(0));
 
+<<<<<<< Updated upstream
     let num_threads = 8;
     let range_per_thread = (max_key_range - min_key_range + 1) / num_threads;
 
@@ -74,4 +91,33 @@ fn main() {
     }
 
     println!("\x1b[38;2;250;128;114m\n[+] Private key not found in the given range!\n\x1b[0m");
+=======
+    //let secp: Arc<Secp256k1<bitcoin::secp256k1::All>> = Arc::new(Secp256k1::new());
+    let keys_checked: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
+    let last_report_time: Arc<Mutex<Instant>> = Arc::new(Mutex::new(Instant::now()));
+    let keys_checked_in_interval: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
+
+    let num_threads = 4u128;
+
+    // ##### EXPERIMENTAL CODE #####
+
+    let secp = Secp256k1::new();
+
+    println!("***** STARTED **********");
+    let current_time = Instant::now();
+
+    let addresses: Vec<_> = (0..4_500)
+        .map(|i| {
+            let secret_key =
+                SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order");
+            let public_key = PublicKey::from_secret_key(&secp, &secret_key);
+            public_key.serialize().to_vec()
+        })
+        .collect();
+
+    let end_time = Instant::now();
+    let total_time = end_time - current_time;
+    println!("Generated {} keys", addresses.len());
+    println!("[Runtime: {:#?}]", total_time);
+>>>>>>> Stashed changes
 }
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 use bitcoin::secp256k1::Secp256k1;
 use bitcoin::util::key::{PublicKey, PrivateKey};
 use bitcoin::network::constants::Network;
@@ -14,10 +15,92 @@ pub fn generate_public_address(secp: &Secp256k1<bitcoin::secp256k1::All>, privat
     let private_key = PrivateKey::from_slice(&hex::decode(private_key_hex).unwrap(), Network::Bitcoin).unwrap();
     let public_key = PublicKey::from_private_key(secp, &private_key);
     let address = Address::p2pkh(&public_key, Network::Bitcoin);
+=======
+#![allow(unused)]
+
+use bitcoin::address::Address;
+use bitcoin::key::{PrivateKey, PublicKey};
+use bitcoin::secp256k1::Secp256k1;
+use bitcoin::Network;
+use num_cpus;
+
+pub fn introduction() {
+    println!("\x1b[38;2;250;128;114m  ╔════════════════════════════════════════════════╗");
+    println!("\x1b[38;2;250;128;114m║\x1b[0m\x1b[1m\x1b[32m          KeryRypper v0.1.2 - Satoshi Quest         \x1b[0m\x1b[38;2;250;128;114m║");
+    println!("\x1b[38;2;250;128;114m║\x1b[0m\x1b[1m\x1b[32m                   by Denzy Legacy                  \x1b[0m\x1b[38;2;250;128;114m║");
+    println!("\x1b[38;2;250;128;114m  ╚════════════════════════════════════════════════╝\x1b[0m");
+}
+
+pub fn machine_info() {
+    let num_cpus: usize = num_cpus::get();
+    println!("\n[+] Host logical cores: {}", num_cpus);
+
+    match sys_info::mem_info() {
+        Ok(info) => {
+            let total_gb: f64 = info.total as f64 / (1024.0 * 1024.0);
+            let free_gb: f64 = info.free as f64 / (1024.0 * 1024.0);
+
+            println!(
+                "[+] Total RAM: {:.2} GB, Free RAM: {:.2} GB",
+                total_gb, free_gb
+            );
+        }
+        Err(e) => {
+            eprintln!("[+] Error retrieving RAM information: {}", e);
+        }
+    }
+
+    let os_type: Result<String, sys_info::Error> = sys_info::os_type();
+    let os_release: Result<String, sys_info::Error> = sys_info::os_release();
+
+    match (os_type, os_release) {
+        (Ok(os), Ok(release)) => {
+            println!("[+] OS: {} v{}", os, release);
+        }
+        (Err(e), _) => {
+            eprintln!("[+] Error retrieving operating system information: {}", e);
+        }
+        (_, Err(e)) => {
+            eprintln!("[+] Error retrieving system version: {}", e);
+        }
+    }
+
+    match sys_info::disk_info() {
+        Ok(disk) => {
+            let total_gb: f64 = disk.total as f64 / (1024.0 * 1024.0);
+            let free_gb: f64 = disk.free as f64 / (1024.0 * 1024.0);
+
+            println!(
+                "[+] Total disk space: {:.2} GB, Free: {:.2} GB\n",
+                total_gb, free_gb
+            );
+        }
+        Err(e) => {
+            eprintln!("[+] Error retrieving disk information: {}\n", e);
+        }
+    }
+}
+
+pub fn generate_public_address(
+    secp: &Secp256k1<bitcoin::secp256k1::All>,
+    private_key_hex: &str,
+) -> String {
+    let private_key_bytes: Vec<u8> = hex::decode(private_key_hex).expect("Invalid hex");
+    let private_key: PrivateKey =
+        PrivateKey::from_slice(&private_key_bytes, Network::Bitcoin).expect("Invalid private key");
+    let public_key: PublicKey = PublicKey::from_private_key(secp, &private_key);
+    let address: Address = Address::p2pkh(&public_key, Network::Bitcoin);
+>>>>>>> Stashed changes
     address.to_string()
 }
 
 pub fn generate_wif(private_key_hex: &str) -> String {
+<<<<<<< Updated upstream
     let private_key = PrivateKey::from_slice(&hex::decode(private_key_hex).unwrap(), Network::Bitcoin).unwrap();
+=======
+    let private_key_bytes: Vec<u8> = hex::decode(private_key_hex).expect("Invalid hex");
+    let private_key: PrivateKey =
+        PrivateKey::from_slice(&private_key_bytes, Network::Bitcoin).expect("Invalid private key");
+>>>>>>> Stashed changes
     private_key.to_wif()
 }
